7c04d10e11f046e67fdb43e56817ddf26890184e
Quentin Sander <02quentin.sander@gulllakecs.org>
2019-02-11 13:01:14 -0500

    add the functions & Variables

--- Functions-Git.py
+++ Functions-Git.py
@@ -1,7 +1,7 @@
 #4.13.3:Greetings
 #Quentin Sander
 #2/5/19
-
+'''
 name = input("What is your name: ")
 
 def greeting():
@@ -9,3 +9,17 @@ def greeting():
     print("Nice to meet you ")
 
 greeting()
+'''
+
+# 4.13.4: Functions and Variables
+#Quentin Sander
+# 2.11.19
+
+x = 406
+
+def print_something():
+    x = 3
+    print('\n', x)
+
+print('\n', x)
+print_something()
